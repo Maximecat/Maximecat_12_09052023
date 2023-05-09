5b0b3a56973aa35a8859020f4dde547a764079ac
Maxime CAT <maximecat@outlook.fr>
2023-05-09 13:12:11 +0200

    All Users Page

---
+++ src/pages/Home/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <a href='/profil/12'>Profil 12</a>
+      <a href='/profil/18'>Profil 18</a>
+    </div>
+  );
+}
+
+export default App;
