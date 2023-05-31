c5258af7515507639b4a31265025251f79bfc1b9
Maxime CAT <maximecat@outlook.fr>
2023-05-31 15:27:50 +0200

    Sidebar component add élements and images

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,9 +1,22 @@
-import './Sidebar.css'
+import './Sidebar.css';
+import SideElement from '../SideElement/SideElement.jsx';
+import meditation from '../../assets/meditation.png';
+import swimming from '../../assets/swimming.png';
+import bike from '../../assets/bike.png';
+import dumbbell from '../../assets/dumbbell.png'
 
 function Sidebar() {
 
     return (
-        <h1>Sidebar</h1>
+        <aside className='asideLayout'>
+            <div className='container-SideElement'>
+                <SideElement logoUrl={meditation} />
+                <SideElement logoUrl={swimming} />
+                <SideElement logoUrl={bike} />
+                <SideElement logoUrl={dumbbell} />
+            </div>
+            <span className='aside-Copyright'>Copyright, SportSee 2020</span>
+        </aside>
     )
 }
 
