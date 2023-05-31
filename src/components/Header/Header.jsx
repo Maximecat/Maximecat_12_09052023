8f51a7ed54b2c7dd05c0466e01191862c49576a2
Maxime CAT <maximecat@outlook.fr>
2023-05-31 13:13:08 +0200

    Header Component add éléments (logo,links)

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,16 @@
 import './Header.css'
+import logo from '../../assets/logo-sportsee.png'
 
 function Header() {
 
     return (
-        <h1>Header</h1>
+        <header className='headerLayout'>
+            <img src={logo} alt="Logo Sportsee" />
+            <a href='#' alt="achanger">Acceuil</a>
+            <a href='#' alt="achangedfr">Profil</a>
+            <a href='#' alt="achangfeer">Réglage</a>
+            <a href='#' alt="achangfeffer">Communauté</a>
+        </header>
     )
 }
 
