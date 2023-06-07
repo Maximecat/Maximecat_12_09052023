b5a37c89d743376471a2dcf3e3e75e54c5dd26be
Maxime CAT <maximecat@outlook.fr>
2023-06-07 14:28:53 +0200

    Profil Page add Component and props + images

--- src/pages/Profil/Profil.jsx
+++ src/pages/Profil/Profil.jsx
@@ -1,9 +1,27 @@
+import Nutriment from '../../components/Nutriment/Nutriment'
 import './Profil.css'
+import Flamme from './Flamme.png'
+import Chicken from './Chicken.png'
+import Apple from './Apple.png'
+import Cheeseburger from './Cheeseburger.png'
 
 function Profil() {
 
     return (
-        <h1>Hello</h1>
+        <div className='profil-main'>
+            <h1>Bonjour <span className='profil-name'>Thomas</span></h1>
+            <p>Félicitation ! Vous avez explosé vos objectifs hier 👏</p>
+            <div className='profil-datas-container'>
+                <div className='graphic-container'></div>
+                <div className='datas-container'>
+                    <Nutriment image={Flamme} background="logo-flamme" value="1,930kCal" unit="Calories" />
+                    <Nutriment image={Chicken} background="logo-chicken" value="155g" unit="Proteines" />
+                    <Nutriment image={Apple} background="logo-apple" value="290g" unit="Glucides" />
+                    <Nutriment image={Cheeseburger} background="logo-cheeseburger" value="50g" unit="Lipides" />
+                </div>
+            </div>
+
+        </div>
     )
 }
 
