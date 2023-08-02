dafc6a349a6d1271298f71828f64ecabf31d6708
Maxime CAT <maximecat@outlook.fr>
2023-08-02 11:29:39 +0200

    API services fetch datas

---
+++ src/utils/services/ApiService.js
@@ -0,0 +1,59 @@
+import User from "../models/User";
+import Activity from "../models/Activity";
+import Session from "../models/Sessions";
+import Performance from "../models/Performance";
+
+class ApiService {
+    apiAdress;
+
+    constructor() {
+        this.apiAdress = "http://localhost:3000/user/";
+    }
+
+    async getUserInformations(userId) {
+        try {
+            const response = await fetch(`${this.apiAdress}${userId}`)
+            const { data } = await response.json()
+
+            return new User(data);
+        } catch (error) {
+            return error
+        }
+    }
+
+    async getUserActivities(userId) {
+        try {
+            const response = await fetch(`${this.apiAdress}${userId}/activity`)
+            const { data } = await response.json()
+
+            return data.sessions.map((session) => new Activity(session))
+        } catch (error) {
+            return error
+        }
+    }
+
+    async getUserAverageSessions(userId) {
+        try {
+            const response = await fetch(`${this.apiAdress}${userId}/average-sessions`)
+            const { data } = await response.json()
+
+            return data.sessions.map((session) => new Session(session))
+        } catch (error) {
+            return error
+        }
+    }
+
+    async getUserPerformances(userId) {
+        try {
+            const response = await fetch(`${this.apiAdress}${userId}/performance`)
+            const { data } = await response.json()
+            console.log(data, data.kind, data.data)
+
+            return data.datas.map((data) => new Performance(data));
+        } catch (error) {
+            return error
+        }
+    }
+}
+
+export default ApiService
