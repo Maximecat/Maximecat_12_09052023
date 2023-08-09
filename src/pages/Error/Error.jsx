aef5885b6f3fa09abcf746f55154765cc6c0ac91
Maxime CAT <maximecat@outlook.fr>
2023-08-09 13:01:39 +0200

    Page Error

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -3,7 +3,14 @@ import './Error.css'
 function Error() {
 
     return (
-        <h1>Error</h1>
+        <div className='error-component'>
+            <h1 className='error-title'>Oups !</h1>
+            <span className='error'>La page que vous recherchez semble introuvable.</span>
+            <span className='error-code'>Code d'erreur: 404</span>
+            <div className='backhome-sentence'> Voici un lien de retour à la
+                <a href='http://localhost:3001/' className='backhome-link'> Page d'acceuil</a>
+            </div>
+        </div>
     )
 }
 
