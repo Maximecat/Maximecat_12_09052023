18decccab8ad9efb467cb7eecc668a191fe9364f
Maxime CAT <maximecat@outlook.fr>
2023-08-09 12:59:35 +0200

    add get() for score

--- src/utils/models/User.js
+++ src/utils/models/User.js
@@ -8,11 +8,22 @@ class User {
 
     constructor(data) {
         this.firstName = data.userInfos.firstName;
-        this.todayScore = data.todayScore;
+        this.todayScore = data.todayScore * 100;
         this.calorieCount = data.keyData.calorieCount;
         this.proteinCount = data.keyData.proteinCount;
         this.carbohydrateCount = data.keyData.carbohydrateCount;
         this.lipidCount = data.keyData.lipidCount;
+    }
+
+    get scoreOftheDay() {
+        return [
+            {
+                todayScore: this.todayScore
+            },
+            {
+                todayScore: 100 - this.todayScore
+            },
+        ]
     }
 }
 
