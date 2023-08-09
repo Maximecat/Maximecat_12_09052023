dbd3ad2d21fe2fab8af916a3e7638155ae143740
Maxime CAT <maximecat@outlook.fr>
2023-08-09 13:02:16 +0200

    Page d'acceuil add user avatar

--- src/pages/Home/App.jsx
+++ src/pages/Home/App.jsx
@@ -1,10 +1,17 @@
 import './App.css';
+import Avatar from '../Profil/Avatar.png'
 
 function App() {
   return (
     <div className="App">
-      <a href='/profil/12'>Profil 12</a>
-      <a href='/profil/18'>Profil 18</a>
+      <a href='/profil/12' className='profil-link'>
+        <img src={Avatar} className='img-profil' alt='' />
+        <span className='user-firstname'>Karl</span>
+      </a>
+      <a href='/profil/18' className='profil-link'>
+        <img src={Avatar} className='img-profil' alt='' />
+        <span className='user-firstname'>Cecilia</span>
+      </a>
     </div>
   );
 }
