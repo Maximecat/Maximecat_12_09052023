24985701778023991bf9865b0d9a0595bcf4e44e
Maxime CAT <maximecat@outlook.fr>
2023-08-09 13:01:10 +0200

    Component Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,10 +6,10 @@ function Header() {
     return (
         <header className='headerLayout'>
             <img className='headerLogo' src={logo} alt="Logo Sportsee" />
-            <a href='#' alt="Page d'acceuil">Acceuil</a>
-            <a href='#' alt="Page Profil">Profil</a>
-            <a href='#' alt="Réglage">Réglage</a>
-            <a href='#' alt="Communauté">Communauté</a>
+            <a href='/' alt="Page d'acceuil">Acceuil</a>
+            <a href='/' alt="Page Profil">Profil</a>
+            <a href='/' alt="Réglage">Réglage</a>
+            <a href='/' alt="Communauté">Communauté</a>
         </header>
     )
 }
