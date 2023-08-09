c35071050fae5d146716b62275d55bca52ca99ef
Maxime CAT <maximecat@outlook.fr>
2023-08-09 12:59:50 +0200

    class Performance

--- src/utils/models/Performance.js
+++ src/utils/models/Performance.js
@@ -2,10 +2,9 @@ class Performance {
     value;
     kind;
 
-    constructor(data, value, kind) {
-        this.value = data.map(value);
-        this.kind = data.kind[kind];
-        console.log(this.kind);
+    constructor(data, kind) {
+        this.value = data.value;
+        this.kind = kind[data.kind];
     }
 }
 
