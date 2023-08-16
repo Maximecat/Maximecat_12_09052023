fec77e3d02fe7463ab874ecf209b1ecd5e34fbcf
Maxime CAT <maximecat@outlook.fr>
2023-08-16 15:35:46 +0200

    Model Performance add translate 'en' to 'fr'

--- src/utils/models/Performance.js
+++ src/utils/models/Performance.js
@@ -4,7 +4,38 @@ class Performance {
 
     constructor(data, kind) {
         this.value = data.value;
-        this.kind = kind[data.kind];
+        this.kind = this.translateKind(kind[data.kind]);
+    }
+
+    translateKind(kind) {
+        switch (kind) {
+            case 'intensity':
+
+                return 'Intensité';
+
+            case 'speed':
+
+                return 'Vitesse';
+
+            case 'strength':
+
+                return 'Force';
+
+            case 'endurance':
+
+                return 'Endurance';
+
+            case 'energy':
+
+                return 'Energie';
+
+            case 'cardio':
+
+                return 'Cardio';
+
+            default:
+                break;
+        }
     }
 }
 
