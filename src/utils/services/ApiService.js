7d7cda5e5ec74817d7370397de06ba3bc7087441
Maxime CAT <maximecat@outlook.fr>
2023-08-23 13:34:19 +0200

    Service add road data mock

--- src/utils/services/ApiService.js
+++ src/utils/services/ApiService.js
@@ -5,14 +5,20 @@ import Performance from "../models/Performance";
 
 class ApiService {
     apiAdress;
+    isMock;
 
     constructor() {
-        this.apiAdress = "http://localhost:3000/user/";
+        this.isMock = false;
+        if (this.isMock === false) {
+            this.apiAdress = "http://localhost:3000/user/";
+        } else {
+            this.apiAdress = "/";
+        }
     }
 
     async getUserInformations(userId) {
         try {
-            const response = await fetch(`${this.apiAdress}${userId}`)
+            const response = await fetch(`${this.apiAdress}${this.isMock ? "index.json" : userId}`)
             const { data } = await response.json()
 
             return new User(data);
@@ -23,7 +29,7 @@ class ApiService {
 
     async getUserActivities(userId) {
         try {
-            const response = await fetch(`${this.apiAdress}${userId}/activity`)
+            const response = await fetch(`${this.apiAdress}${this.isMock ? "activity.json" : userId + "/activity"}`)
             const { data } = await response.json()
 
             return data.sessions.map((session) => new Activity(session))
@@ -34,7 +40,7 @@ class ApiService {
 
     async getUserAverageSessions(userId) {
         try {
-            const response = await fetch(`${this.apiAdress}${userId}/average-sessions`)
+            const response = await fetch(`${this.apiAdress}${this.isMock ? "average-sessions.json" : userId + "/average-sessions"}`)
             const { data } = await response.json()
 
             return data.sessions.map((session) => new Session(session))
@@ -45,10 +51,12 @@ class ApiService {
 
     async getUserPerformances(userId) {
         try {
-            const response = await fetch(`${this.apiAdress}${userId}/performance`)
+            const response = await fetch(`${this.apiAdress}${this.isMock ? "performance.json" : userId + "/performance"}`)
             const { data } = await response.json()
 
-            return data.data.map((perf) => new Performance(perf, data.kind));
+            return data.data
+                .map((perf) => new Performance(perf, data.kind))
+                .sort((perfA, perfB) => perfA.order - perfB.order);
         } catch (error) {
             return error
         }
