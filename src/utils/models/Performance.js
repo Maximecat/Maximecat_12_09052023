bdd00141a76d1679e0cb7984e463e0bac217a05e
Maxime CAT <maximecat@outlook.fr>
2023-08-23 13:33:29 +0200

    Model Performance add order

--- src/utils/models/Performance.js
+++ src/utils/models/Performance.js
@@ -1,6 +1,7 @@
 class Performance {
     value;
     kind;
+    order;
 
     constructor(data, kind) {
         this.value = data.value;
@@ -10,29 +11,23 @@ class Performance {
     translateKind(kind) {
         switch (kind) {
             case 'intensity':
-
+                this.order = 1
                 return 'Intensité';
-
             case 'speed':
-
+                this.order = 2
                 return 'Vitesse';
-
             case 'strength':
-
+                this.order = 3
                 return 'Force';
-
             case 'endurance':
-
+                this.order = 4
                 return 'Endurance';
-
             case 'energy':
-
+                this.order = 5
                 return 'Energie';
-
             case 'cardio':
-
+                this.order = 6
                 return 'Cardio';
-
             default:
                 break;
         }
