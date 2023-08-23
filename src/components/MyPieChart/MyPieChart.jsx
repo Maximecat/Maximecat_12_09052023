b01b1747aa999a3c61d072b013664462d5ff6347
Maxime CAT <maximecat@outlook.fr>
2023-08-23 13:37:19 +0200

    Component PieChart

---
+++ src/components/MyPieChart/MyPieChart.jsx
@@ -0,0 +1,28 @@
+import { Cell, Legend, Pie, PieChart, ResponsiveContainer, Tooltip } from 'recharts';
+import './MyPieChart.css'
+
+function MyPieChart({ user }) {
+    return (
+        <div className='fourth-chart'>
+            <ResponsiveContainer>
+                <PieChart>
+                    <Legend content={() => "Score"} verticalAlign="top" align='left' wrapperStyle={{ color: '#20253A', fontFamily: 'Roboto', fontSize: 12, fontWeight: 600, marginTop: 15, marginLeft: 20 }} />
+                    <Pie data={user.scoreOftheDay} dataKey={'todayScore'} innerRadius={66} startAngle={90} endAngle={450} cornerRadius='50%'>
+                        <Cell fill="#FF0000" stroke="#FF0000" />
+                        <Cell fill="transparent" stroke="transparent" />
+                    </Pie>
+                    <Tooltip />
+                </PieChart>
+            </ResponsiveContainer>
+            <div className='score-label'>
+                <span className='score'>{user.todayScore || user.score}%</span>
+                <p className='score-fade'>
+                    de votre
+                    <br />objectif
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default MyPieChart;
