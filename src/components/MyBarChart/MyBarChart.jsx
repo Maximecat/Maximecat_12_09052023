35e3b98f49745def74315fdcdfd9ba5116713dc6
Maxime CAT <maximecat@outlook.fr>
2023-08-26 21:53:44 +0200

    Component BarChart

--- src/components/MyBarChart/MyBarChart.jsx
+++ src/components/MyBarChart/MyBarChart.jsx
@@ -9,7 +9,7 @@ function MyBarChart({ activities }) {
             <ResponsiveContainer height={230}>
                 <BarChart data={activities} margin={{ left: 40, top: 10 }} barGap={6} >
                     <CartesianGrid strokeDasharray={2} horizontal={true} vertical={false} strokeWidth={0.4} />
-                    <Bar dataKey='kilogram' name='Poids (kg)' radius={[5, 5, 0, 0]} barSize={7} fill='#282D30' />
+                    <Bar dataKey='kilogram' name='Poids (kg)' radius={[5, 5, 0, 0]} barSize={7} />
                     <Bar dataKey='calories' name='Calories Brûlées (kCal)' radius={[5, 5, 0, 0]} barSize={7} fill='#E60000' />
                     <XAxis dataKey='day' tickSize={0} tickMargin={20} strokeWidth={0.2} fillOpacity={0.8} padding={{ left: -40, right: -40 }} />
                     <YAxis orientation='right' tickMargin={10} strokeWidth={0} />
