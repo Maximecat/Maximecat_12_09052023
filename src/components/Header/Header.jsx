ee4d7c636bcab03a7e57760e42db905e8005f6cd
Maxime CAT <maximecat@outlook.fr>
2023-12-27 14:34:43 +0100

    Header component update orthography

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,9 +6,9 @@ function Header() {
     return (
         <header className='headerLayout'>
             <img className='headerLogo' src={logo} alt="Logo Sportsee" />
-            <a href='/' alt="Page d'acceuil">Acceuil</a>
+            <a href='/' alt="Page d'acceuil">Accueil</a>
             <a href='/' alt="Page Profil">Profil</a>
-            <a href='/' alt="Réglage">Réglage</a>
+            <a href='/' alt="Réglage">Réglages</a>
             <a href='/' alt="Communauté">Communauté</a>
         </header>
     )
